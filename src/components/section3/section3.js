385f4fa8a4eca40a82ad6a14aec3bc383f01c7e0
Vikash Kumar <vikashkumar.cse19@chitkarauniversity.edu.in>
2021-09-20 17:16:21 +0530

    section3

--- src/components/section3/section3.js
+++ src/components/section3/section3.js
@@ -1,7 +1,32 @@
-const esction3 = () => {
-    return ( 
-        <h1>section3</h1>
-     );
+import './section3.css';
+import Img1 from './phone1.png';
+import Img2 from './phone2.png';
+import Img3 from './astore.png';
+import Img4 from './gplay.png';
+
+const section3 = () => {
+    return (
+        <div className="section3">
+            <div className="outterdiv">
+                <h1>
+                    Restaurants in <br /> your pocket
+                </h1>
+                <p>
+                    Order from your favorite restaurants &amp; track on the go, with the all-new Swiggy app.
+                </p>
+                <div className="buttons">
+                    <a href="/" target="_blank" rel="noopener noreferrer">
+                        <img src={Img4} alt="" />
+                    </a>
+                    <a href="/" target="_blank" rel="noopener noreferrer">
+                        <img src={Img3} alt="" />
+                    </a>
+                </div>
+            </div>
+            <img src={Img1} alt="" />
+            <img src={Img2} alt="" />
+        </div>
+    );
 }
- 
-export default esction3;
+
+export default section3;
